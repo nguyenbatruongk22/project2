f92cc7cddbcdb45f297d6a38f9f2958baa5589d2
nguyenbatruongk22 <chanhchua1301@gmail.com>
2024-11-11 20:55:31 +0700

    pj2

--- K22CNTT3-NguyenBaTruong-project2/Controllers/KHACH_HANGController.cs
+++ K22CNTT3-NguyenBaTruong-project2/Controllers/KHACH_HANGController.cs
@@ -1,127 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using K22CNTT3_NguyenBaTruong_project2.Models;
 using System.Linq;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using K22CNTT3_NguyenBaTruong_project2.Models;
+using System.Web.Security;
 
-namespace K22CNTT3_NguyenBaTruong_project2.Controllers
+public class KHACH_HANGController : Controller
 {
-    public class KHACH_HANGController : Controller
+    private PhukienthucungEntities db = new PhukienthucungEntities();
+
+    // GET: KHACH_HANG/Login
+    public ActionResult Login()
     {
-        private PhukienthucungEntities db = new PhukienthucungEntities();
+        return View();
+    }
 
-        // GET: KHACH_HANG
-        public ActionResult Index()
+    // POST: KHACH_HANG/Login
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Login(string Tai_khoan, string Mat_khau)
+    {
+        if (string.IsNullOrWhiteSpace(Tai_khoan) || string.IsNullOrWhiteSpace(Mat_khau))
         {
-            return View(db.KHACH_HANG.ToList());
-        }
-
-        // GET: KHACH_HANG/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            KHACH_HANG kHACH_HANG = db.KHACH_HANG.Find(id);
-            if (kHACH_HANG == null)
-            {
-                return HttpNotFound();
-            }
-            return View(kHACH_HANG);
-        }
-
-        // GET: KHACH_HANG/Create
-        public ActionResult Create()
-        {
+            ModelState.AddModelError("", "Tài khoản và mật khẩu không được để trống");
             return View();
         }
 
-        // POST: KHACH_HANG/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "MaKH,Ho_ten,Tai_khoan,Mat_khau,Dia_chi,Dien_thoai,Email,Ngay_sinh,Ngay_cap_nhat,Gioi_tinh,Tich_diem,Trang_thai")] KHACH_HANG kHACH_HANG)
-        {
-            if (ModelState.IsValid)
-            {
-                db.KHACH_HANG.Add(kHACH_HANG);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+        // Check user credentials
+        var user = db.KHACH_HANG.FirstOrDefault(kh => kh.Tai_khoan == Tai_khoan && kh.Mat_khau == Mat_khau);
 
-            return View(kHACH_HANG);
-        }
-
-        // GET: KHACH_HANG/Edit/5
-        public ActionResult Edit(int? id)
+        if (user != null)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            KHACH_HANG kHACH_HANG = db.KHACH_HANG.Find(id);
-            if (kHACH_HANG == null)
-            {
-                return HttpNotFound();
-            }
-            return View(kHACH_HANG);
+            FormsAuthentication.SetAuthCookie(user.Tai_khoan, false);
+            return RedirectToAction("Index", "Home");
         }
-
-        // POST: KHACH_HANG/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "MaKH,Ho_ten,Tai_khoan,Mat_khau,Dia_chi,Dien_thoai,Email,Ngay_sinh,Ngay_cap_nhat,Gioi_tinh,Tich_diem,Trang_thai")] KHACH_HANG kHACH_HANG)
+        else
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(kHACH_HANG).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(kHACH_HANG);
+            ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không đúng");
+            return View();
         }
+    }
 
-        // GET: KHACH_HANG/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            KHACH_HANG kHACH_HANG = db.KHACH_HANG.Find(id);
-            if (kHACH_HANG == null)
-            {
-                return HttpNotFound();
-            }
-            return View(kHACH_HANG);
-        }
-
-        // POST: KHACH_HANG/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            KHACH_HANG kHACH_HANG = db.KHACH_HANG.Find(id);
-            db.KHACH_HANG.Remove(kHACH_HANG);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+    // GET: KHACH_HANG/Logout
+    public ActionResult Logout()
+    {
+        FormsAuthentication.SignOut();
+        return RedirectToAction("Login");
     }
 }
